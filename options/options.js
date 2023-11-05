03e21d3911c29210ecbc2e83fa16ef35690341f8
Crowdbotics <team@crowdbotics.com>
2023-11-05 04:21:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mute-darkness-44344",
+  url: "https://mute-darkness-44344.botics.co",
+  api: "https://mute-darkness-44344.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
